4b86d43f343ad835905db972aa16bdc8adef8723
fribeiro <fabiomatcomp@gmail.com>
2020-12-01 11:59:04 +0000

    data stream works: connector and producer changeset

---
+++ code-sample/project_samples/tests/producer_test.py
@@ -0,0 +1,88 @@
+# Please complete the TODO items in the code
+
+from dataclasses import dataclass, field
+import json
+import random
+
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+from faker import Faker
+
+
+faker = Faker()
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+TOPIC_NAME = "org.udacity.exercise4.purchases"
+
+
+def produce(topic_name):
+    """Produces data synchronously into the Kafka Topic"""
+    #
+    # TODO: Configure the Producer to:
+    #       1. Have a Client ID
+    #       2. Have a batch size of 100
+    #       3. A Linger Milliseconds of 1 second
+    #       4. LZ4 Compression
+    #
+    #       See: https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    #
+    p = Producer(
+        {
+            "bootstrap.servers": BROKER_URL,
+            "client.id": "ex.4",
+            "batch.num.messages": 1000,
+            # "queue.buffering.max.messages": 10000,
+            "linger.ms": 1000,
+            "compression.type": "lz4"
+        }
+    )
+
+    iter = 0
+    while True:
+        p.produce(topic_name, Purchase().serialize())
+        if iter % 100 == 0:
+            print(f"Message sent {iter}")
+        iter += 1
+        p.poll(0)
+
+def main():
+    """Checks for topic and creates the topic if it does not exist"""
+    create_topic(TOPIC_NAME)
+    try:
+        produce(TOPIC_NAME)
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+
+def create_topic(client):
+    """Creates the topic with the given topic name"""
+    client = AdminClient({"bootstrap.servers": BROKER_URL})
+    futures = client.create_topics(
+        [NewTopic(topic=TOPIC_NAME, num_partitions=5, replication_factor=1)]
+    )
+    for _, future in futures.items():
+        try:
+            future.result()
+        except Exception as e:
+            pass
+
+
+@dataclass
+class Purchase:
+    username: str = field(default_factory=faker.user_name)
+    currency: str = field(default_factory=faker.currency_code)
+    amount: int = field(default_factory=lambda: random.randint(100, 200000))
+
+    def serialize(self):
+        """Serializes the object in JSON string format"""
+        return json.dumps(
+            {
+                "username": self.username,
+                "currency": self.currency,
+                "amount": self.amount,
+            }
+        )
+
+
+if __name__ == "__main__":
+    main()
--- project-1/starter/producers/models/producer.py
+++ project-1/starter/producers/models/producer.py
@@ -2,17 +2,18 @@
 import logging
 import time
 
-
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
 
+BROKER_URL = 'PLAINTEXT://localhost:9092'
+SCHEMA_REGISTRY_URL = 'http://localhost:8081'
+
 
 class Producer:
     """Defines and provides common functionality amongst Producers"""
-
     # Tracks existing topics across all Producer instances
     existing_topics = set([])
 
@@ -31,16 +32,9 @@ class Producer:
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            'bootstrap.servers': BROKER_URL,
+            'schema.registry.url': SCHEMA_REGISTRY_URL,
         }
 
         # If the topic does not already exist, try to create it
@@ -48,31 +42,36 @@ class Producer:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(
+            self.broker_properties
+        )
+
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        client = AdminClient(
+            {'bootstrap.servers': BROKER_URL}
+        )
+        futures = client.create_topics(
+            [NewTopic(
+                topic=self.topic_name,
+                num_partitions=self.num_partitions,
+                replication_factor=self.num_replicas)]
+        )
+        for _, future in futures.items():
+            try:
+                future.result()
+            except:
+                logger.info(
+                    "topic creation kafka integration incomplete - skipping"
+                )
 
-    def time_millis(self):
-        return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        logger.info("closing producer")
+        self.producer.flush()
+
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
