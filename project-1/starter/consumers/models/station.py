b7d8ce364c207ca8c8d474cef7d292bf99c37442
Fabio Ribeiro <fabiomatcomp@gmail.com>
2021-02-08 21:05:41 +0000

    data streaming nano

--- project-1/starter/consumers/models/station.py
+++ project-1/starter/consumers/models/station.py
@@ -40,4 +40,5 @@ class Station:
 
     def process_message(self, json_data):
         """Handles arrival and turnstile messages"""
+        logging.info(f"count value => {json_data['COUNT']}")
         self.num_turnstile_entries = json_data["COUNT"]
